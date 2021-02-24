357b67442af82412bc2f807bafdd291edb9dfa00
rockbeen <rodel.rodel.rodel@mail.ru>
2021-02-25 00:04:52 +0300

    second

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton_0,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_1,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_3,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_4,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_5,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_6,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_7,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_8,SIGNAL(clicked()),this,SLOT(what_number()));
+    connect(ui->pushButton_9,SIGNAL(clicked()),this,SLOT(what_number()));
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::what_number()
+{
+    QPushButton *button=(QPushButton *)sender();
+    ui->decision->setText(button->text());
+}
--- mainwindow.h
+++ mainwindow.h
@@ -17,5 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+private slots:
+    void what_number();
 };
 #endif // MAINWINDOW_H
